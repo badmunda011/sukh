9415bc1bc4fbffb56790fbbfe5474a559029de07
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-09-08 11:21:55 +0530

    Update and rename vps_config.py to config.py

--- config.py
+++ config.py
@@ -10,10 +10,10 @@ class Development(Config):
     
     APP_ID = 27383453 # 666666 is a placeholder. Fill your 6 digit api id
     API_HASH = "4c246fb0c649477cc2e79b6a178ddfaa"  # replace this with your api hash
-    BOT_TOKEN = "6837458112:AAG5XLx5p5NN9_ljbgsV4tg8yfaI76EiL7Q"  # Create a bot from @BotFather and paste the token here
+    BOT_TOKEN = "7451378287:AAHmSqw8cIDKo4UmImAK2opVQkZIm4ArXjg"  # Create a bot from @BotFather and paste the token here
     BOT_LIBRARY = "telethon"  # fill 'pyrogram' if you want pyrogram version of Pbxbot else leave it as it is.
-    DATABASE_URL = "mongodb+srv://BADMUNDA:BADMYDAD@badhacker.i5nw9na.mongodb.net/"  # A postgresql database url from elephantsql
-    PBXBOT_SESSION = "Your value"  # telethon or pyrogram string according to BOT_LIBRARY
+    DATABASE_URL = "postgres://lwktrpsc:1UlTfWHhnxKZWRETlLu6AF0x4AsyKXhO@rosie.db.elephantsql.com/lwktrpsc"  # A postgresql database url from elephantsql
+    PBXBOT_SESSION = "hsjns"  # telethon or pyrogram string according to BOT_LIBRARY
     HANDLER = "®"  # Custom Command Handler
     SUDO_HANDLER = "©"  # Custom Command Handler for sudo users.
 
