9b1ea673fdd5b391db7b419cad16bc2127f9f062
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-09-08 11:30:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Development(Config):
     BOT_TOKEN = "7451378287:AAHmSqw8cIDKo4UmImAK2opVQkZIm4ArXjg"  # Create a bot from @BotFather and paste the token here
     BOT_LIBRARY = "telethon"  # fill 'pyrogram' if you want pyrogram version of Pbxbot else leave it as it is.
     DATABASE_URL = "postgres://lwktrpsc:1UlTfWHhnxKZWRETlLu6AF0x4AsyKXhO@rosie.db.elephantsql.com/lwktrpsc"  # A postgresql database url from elephantsql
-    PBXBOT_SESSION = "hsjns"  # telethon or pyrogram string according to BOT_LIBRARY
+    PBXBOT_SESSION = "==heLL1BVtsOIIBu63krgyOCV-iHfwYI8Eo9VX7N2KYDCVgucf8m9GN2exTobLSwgi2OSrlXMXhQtUY0u9DtKFoUl934nFlbArRZ3SLRIQWhtKRC_PSAu7d4d4d5geO2nUhIik3D0La3fZrJ6ySSDXUM8BPlkPygZVCJ17FBM1daYpOqPvd3teNzQdJd3WpqM33tT3mwqez3T4mdryVgR4eD8PRBygmUJMGcgg--fb_TfOxHvqoIZOKzLMQ-5Lr33SMdFp8cYyX_v4EhFqWzXLUdyYwl8J6wWZJopw5mDdfhWvuKfpb8vGz_SvZQL0NJveBK8QT15Yl35bCSRJrjUEKFjOtCyvWU_WHpdM=BOt=="  # telethon or pyrogram string according to BOT_LIBRARY
     HANDLER = "®"  # Custom Command Handler
     SUDO_HANDLER = "©"  # Custom Command Handler for sudo users.
 
